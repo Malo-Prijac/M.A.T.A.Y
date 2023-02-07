27bfac66fef4ac94c679bf272efcca26354f8788
Malo-Prijac <106429819+Malo-Prijac@users.noreply.github.com>
2023-02-07 16:32:33 +0100

    Fix Desert

--- Assets/New/Scripts/Interraction/SwitchTreeStatue.cs
+++ Assets/New/Scripts/Interraction/SwitchTreeStatue.cs
@@ -7,13 +7,14 @@ public class SwitchTreeStatue : MonoBehaviour
 {
     [SerializeField] private GameObject Tree;
     [SerializeField] private GameObject Statue;
-
+    [SerializeField] private GameObject DisabledStatue;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             Tree.SetActive(false);
             Statue.SetActive(true);
+            DisabledStatue.SetActive(false);
         }
     }
 }
